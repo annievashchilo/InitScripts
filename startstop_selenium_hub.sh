f056491eb11fb2ece1f02807d1d5497ce059d674
idancie <melanthium@gmail.com>
2015-07-09 11:10:25 +0300

    Add init scripts for starting Selenium Grid

---
+++ startstop_selenium_hub.sh
@@ -0,0 +1,77 @@
+#!/bin/bash  
+                                                                                                                                                                                                                   
+DESC="Selenium Grid Server"
+RUN_AS="selenium"
+JAVA_BIN="/usr/bin/java"
+ 
+SELENIUM_DIR="/automation/00007167/TestAutomationFramework/selenium-taf"
+PID_FILE="$SELENIUM_DIR/selenium-grid.pid"
+JAR_FILE="$SELENIUM_DIR/selenium-server-standalone.jar"
+LOG_DIR="/var/log/selenium"
+LOG_FILE="${LOG_DIR}/selenium-grid.log"
+ 
+USER="selenium"
+GROUP="selenium"
+ 
+MAX_MEMORY="-Xmx256m"
+STACK_SIZE="-Xss8m"
+
+while getopts p:c: option
+do
+    case "${option}" in
+        p) PORT=${OPTARG};;
+        c) COMMAND=${OPTARG};;
+    esac
+done
+ 
+DAEMON_OPTS=" $MAX_MEMORY $STACK_SIZE -jar $JAR_FILE -role hub -port $PORT -hubConfig hubconfig.json
+ 
+NAME="selenium"
+ 
+if [ "$1" != status ]; then
+    if [ ! -d ${LOG_DIR} ]; then
+        mkdir --mode 750 --parents ${LOG_DIR}
+        chown ${USER}:${GROUP} ${LOG_DIR}
+    fi  
+fi
+ 
+
+
+ 
+# TODO: Put together /etc/init.d/xvfb
+# export DISPLAY=:99.0
+ 
+ 
+case $COMMAND in
+    start)
+        echo -n "Starting $DESC: "
+        if start-stop-daemon -c $RUN_AS --start --background --pidfile $PID_FILE --make-pidfile --exec $JAVA_BIN -- $DAEMON_OPTS ; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+        fi
+        ;;
+ 
+    stop)
+        echo -n "Stopping $DESC: "
+        start-stop-daemon --stop --pidfile $PID_FILE
+        echo "$NAME."
+        ;;
+ 
+    restart|force-reload)
+        echo -n "Restarting $DESC: "
+        start-stop-daemon --stop --pidfile $PID_FILE
+        sleep 1
+        start-stop-daemon -c $RUN_AS --start --background --pidfile $PID_FILE  --make-pidfile --exec $JAVA_BIN -- $DAEMON_OPTS
+        echo "$NAME."
+        ;;
+ 
+    status)
+        status_of_proc -p "$PID_FILE" "$DAEMON" "selenium" && exit 0 || exit $?
+        ;;
+ 
+    *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $N -p HUB_PORT -c COMMAND {start|stop|restart|force-reload|status}" >&2
+        exit 1
+        ;;
---
+++ startstop_selenium_node.sh
@@ -0,0 +1,96 @@
+#!/bin/bash   
+
+while getopts h:H:p:c: option
+do
+    case "${option}" in
+        h) HUB=${OPTARG};;
+        H) HUB_PORT=${OPTARG};;
+        p) PORT=${OPTARG};;
+        c) COMMAND=${OPTARG};;
+    esac
+done
+
+                                                                                                                                                                                                                  
+DESC="Selenium Grid Server"
+RUN_AS="selenium"
+JAVA_BIN="/usr/bin/java"
+HUBURL="http://${HUB}:${HUB_PORT}/grid/register"
+ 
+SELENIUM_DIR="/automation/00007167/TestAutomationFramework/selenium-taf"
+PID_FILE="$SELENIUM_DIR/selenium-node.pid"
+JAR_FILE="$SELENIUM_DIR/selenium-server-standalone.jar"
+LOG_DIR="/var/log/selenium"
+LOG_FILE="${LOG_DIR}/selenium-grid.log"
+ 
+USER="selenium"
+GROUP="selenium"
+ 
+MAX_MEMORY="-Xmx256m"
+STACK_SIZE="-Xss8m"
+
+#java -jar selenium-server-standalone-2.37.0.jar -role webdriver -hub http://10.153.30.102:4412/grid/register -port 4412
+ 
+DAEMON_OPTS=" -client $MAX_MEMORY $STACK_SIZE -jar $JAR_FILE -role webdriver -hub ${HUBURL} -log $LOG_FILE" -port ${PORT}
+ 
+DISPLAY_PORT=501
+XVFB="/usr/bin/Xvfb"
+XVFB_OPTS=" :${DISPLAY_PORT} -screen 0 1024x768x24"
+XVFB_PID_FILE="$SELENIUM_DIR/xvfb-node.pid"
+
+ 
+NAME="Selenium Node"
+ 
+if [ $COMMAND != status ]; then
+    if [ ! -d ${LOG_DIR} ]; then
+        mkdir --mode 750 --parents ${LOG_DIR}
+        chown ${USER}:${GROUP} ${LOG_DIR}
+    fi  
+fi
+ 
+. /lib/lsb/init-functions
+ 
+case $COMMAND in
+    start)
+        log_daemon_msg "Starting ${DESC}: " "Xvfb"
+        if start-stop-daemon -c $RUN_AS --start --background --pidfile $XVFB_PID_FILE --make-pidfile --exec $XVFB -- $XVFB_OPTS ; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+            exit 1
+        fi
+ 
+        export DISPLAY=:${DISPLAY_PORT}.0
+        log_daemon_msg "Starting ${DESC}: " $NAME
+        echo "Starting ${DESC}: " $NAME
+        if start-stop-daemon -c $RUN_AS --start --background --pidfile $PID_FILE --make-pidfile --exec $JAVA_BIN -- $DAEMON_OPTS ; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+        fi
+        ;;
+ 
+    stop)
+        echo -n "Stopping $DESC: "
+        start-stop-daemon --stop --pidfile $XVFB_PID_FILE
+        start-stop-daemon --stop --pidfile $PID_FILE
+        echo "$NAME."
+        ;;
+ 
+    restart|force-reload)
+        echo -n "Restarting $DESC: "
+        start-stop-daemon --stop --pidfile $PID_FILE
+        sleep 1
+        start-stop-daemon -c $RUN_AS --start --background --pidfile $PID_FILE  --make-pidfile --exec $JAVA_BIN -- $DAEMON_OPTS
+        echo "$NAME."
+        ;;
+ 
+    status)
+        status_of_proc -p "$XVFB_PID_FILE" "$DAEMON" "Xvfb" && status_of_proc -p "$PID_FILE" "$DAEMON" "Selenium node" && exit 0 || exit $?
+        ;;
+ 
+    *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $N -h HUB -H HUB_PORT -p NODE_PORT -c COMMAND {start|stop|restart|force-reload|status}" >&2
+        exit 1
+        ;;
+esac
